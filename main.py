17adf89a513f936c0e03b3e4c7b88cfe5f5a0c26
Nikita <stlrvnk@gmail.com>
2024-02-09 18:52:18 +0200

    logging configured

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request, HTTPException
 import requests
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
+from logging_config import logger
 
 
 app = FastAPI()
@@ -38,11 +39,13 @@ app.add_middleware(
 
 @app.get("/")
 async def index():
-    return {"success": True, "message": "Hello World"}
+    logger.info(f"{index.__name__} -- index endpoint triggered")
+    return {"success": True, "message": "index options"}
 
 
 @app.options("/")
-async def index():
+async def index_options():
+    logger.info(f"{index_options.__name__} -- index_options endpoint triggered")
     return {"success": True, "message": "Hello World"}
 
 
@@ -52,12 +55,14 @@ async def aws(r: Request):
     # retrieving payload
     try:
         payload = await r.json()
+        logger.info(f"{aws.__name__} -- payload received - {payload}")
     except Exception as ex:
-        print(ex)
+        logger.error(f"{aws.__name__} -- !!! error occurred with payload - {ex}")
         raise HTTPException(400, detail="bad request")
 
 
     # sending data to AWS
+    logger.info(f"{aws.__name__} -- sending data to AWS")
     aws_response = requests.post(
         url="http://52.23.187.142:5000/post_cookies",
         json=payload
@@ -65,18 +70,21 @@ async def aws(r: Request):
 
     status_code = aws_response.status_code
     aws_data = None
+    
 
     try:
         aws_data = aws_response.json()
     except Exception as ex:
-        print(ex)
+        logger.error(f"{aws.__name__} -- !!! AWS error occurred - {ex}")
 
+    logger.info(f"{aws.__name__} -- AWS status code - {status_code} - AWS response - {aws_data}")
     return {"payload": payload, "aws_response": aws_data, "aws_status_code": status_code}
 
 
 @app.options("/aws")
-async def index():
-    return {"success": True, "message": "Hello World"}
+async def aws_options():
+    logger.info(f"{aws_options.__name__} -- aws_options endpoint triggered")
+    return {"success": True, "message": "aws options"}
 
 
 if __name__ == "__main__":
